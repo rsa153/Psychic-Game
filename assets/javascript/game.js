f40e88029fd8cda0ced51b19ca62ec3c364c7c45
rsa153 <r-saber@nlaw.northwestern.edu>
2018-12-16 15:29:47 -0600

    Final commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,47 @@
+
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
+    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessSoFar =[];
+
+
+document.onkeyup = function(event) {
+        var userGuess = event.key;
+        var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];                
+        guessSoFar.push(userGuess);
+
+if (guesses > 0) {
+    if (userGuess === computerChoice) {
+            wins++;
+            guesses = 9;
+            guessSoFar = [];
+            console.log(computerChoice);
+        console.log(userGuess);
+        console.log(wins);
+        console.log(losses);
+        }
+        
+    if (userGuess !== computerChoice) {
+            guesses--;
+        } 
+        console.log(computerChoice);
+        console.log(userGuess);
+        console.log(wins);
+        console.log(losses);
+    }
+ if (guesses === 0) {
+        losses++;
+        guesses = 9;
+        guessSoFar = [];
+        
+    }
+    
+document.querySelector("#wins-text").innerHTML = "Wins: " + wins;
+document.querySelector("#losses-text").innerHTML = "Losses: " + losses;
+document.querySelector("#guesses-text").innerHTML = "Guesses Left: " + guesses;
+document.querySelector("#guesses-sofar-text").innerHTML = "Your Guesses So Far: " + guessSoFar;                        
+
+} 
